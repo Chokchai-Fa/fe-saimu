d0948919fc59e03fc62b61eea38cdbb03358d729
Chokchai-Fa <chokchai_f@muangthai.co.th>
2024-04-09 00:11:49 +0700

    format doc

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,21 +1,19 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Image from 'react-bootstrap/Image';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
 
-import './index.css'
-import logo from '../../assets/logo.png'
-import background from '../../assets/background.png'
-import { useState } from 'react';
+import "./index.css";
+import logo from "../../assets/logo.png";
+import background from "../../assets/background.png";
+import { useState } from "react";
 
-
-const LoginScreen = () =>{
-
-  const  [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
@@ -24,68 +22,60 @@ const LoginScreen = () =>{
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(email, password)
+    console.log(email, password);
   };
 
-    return (
-      <div>
-        <div className='container' 
-          style={{
-            backgroundImage: `url(${background})` 
-          }}
-        >
-          <div className='inner-container'>
-           <div className='header'>
-            <Image className= "logo" src = {logo} />
-           <h1>SaiMU</h1>
+  return (
+    <div>
+      <div
+        className="container"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
+        <div className="inner-container">
+          <div className="header">
+            <Image className="logo" src={logo} />
+            <h1>SaiMU</h1>
           </div>
           <div>
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            value = {email}
-            onChange={handleEmailChange}  
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            value = {password}
-            onChange={handlePasswordChange}  
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me for Good Fortune" />
-        </Form.Group>
-        <Button 
-          variant="danger" 
-          type="submit"
-          onClick={handleSubmit}  
-        >
-          Login
-        </Button>
-      </Form>
-      </div>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me for Good Fortune" />
+              </Form.Group>
+              <Button variant="danger" type="submit" onClick={handleSubmit}>
+                Login
+              </Button>
+            </Form>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-    )
+    </div>
+  );
+};
 
-}
-
-
-const style = {
-    
-}
+const style = {};
 
 export default LoginScreen;
